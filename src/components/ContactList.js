7c822f819bd5ea2b1efd6c5201573fc61012d15e
Kat <kemetcalf3.14@gmail.com>
2023-03-23 19:00:24 -0700

    Removal of remaining create-react-app boiler plate and Semantic UI install

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -3,33 +3,29 @@ import { Link } from "react-router-dom";
 import ContactCard from "./ContactCard";
 
 const ContactList = (props) => {
-  console.log(props);
+	const deleteContactHandler = (id) => {
+		props.getContactId(id);
+	};
 
-  const deleteContactHandler = (id) => {
-    props.getContactId(id);
-  };
+	const renderContactList = props.contacts.map((contact) => {
+		return (
+			<ContactCard
+				contact={contact}
+				clickHandler={deleteContactHandler}
+				key={contact.id}
+			/>
+		);
+	});
 
-  const renderContactList = props.contacts.map((contact) => {
-    return (
-      <ContactCard
-        contact={contact}
-        clickHandler={deleteContactHandler}
-        key={contact.id}
-      />
-    );
-  });
-
-  return (
-    <div className="main">
-      <h2 style={{ marginTop: "50px" }}>
-        Contact list
-        <Link to="/add">
-          <button className="ui button blue right">Add Contact</button>
-        </Link>
-      </h2>
-      <div className="ui celled list">{renderContactList}</div>
-    </div>
-  );
+	return (
+		<div className="main">
+			<h2 style={{ marginTop: "50px" }}>Contact list</h2>
+			<Link to="/add">
+				<button className="ui button blue right">Add Contact</button>
+			</Link>
+			<div className="ui celled list">{renderContactList}</div>
+		</div>
+	);
 };
 
 export default ContactList;
