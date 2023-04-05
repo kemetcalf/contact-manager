2b5653680488bf7c07ef23804006ebc48f6013ee
Kat <kemetcalf3.14@gmail.com>
2023-04-04 23:04:33 -0700

    Image and better formatting on ConfirmDelete

--- src/components/ConfirmDelete.js
+++ src/components/ConfirmDelete.js
@@ -1,25 +1,40 @@
-import { Header, Icon } from "semantic-ui-react";
+import { Button, Container, Header, Icon, Image } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+import user from "../images/user.png";
 
 const ConfirmDelete = (props) => {
 	const { id, name, email } = props.location.state.contact;
+
 	console.log(props.location.state.contact);
+
 	const deleteContactHandler = (id) => {
 		props.getContactId(id);
 	};
+
 	return (
 		<div>
-			<Header as="h2">
-				Are you sure you want to delete {name}? {id}
+			<Header as="h2" textAlign="center">
+				Are you sure you want to <br /> delete {name}'s contact info?
 			</Header>
-			{/* <ContactDetail /> */}
-			<Link to="/">
-				<Icon
-					name="trash alternate outline"
-					color="red"
-					onClick={() => deleteContactHandler(id)}
-				/>
-			</Link>
+			<Container textAlign="center">
+				<Image src={user} as="a" inline />
+			</Container>
+
+			<Container textAlign="center">
+				<Link to="/">
+					<Button color="blue" circular icon>
+						<Icon name="arrow alternate circle left outline" />
+					</Button>
+				</Link>
+				<Link to="/">
+					<Icon
+						name="trash alternate outline"
+						color="red"
+						size="big"
+						onClick={() => deleteContactHandler(id)}
+					/>
+				</Link>
+			</Container>
 		</div>
 	);
 };
